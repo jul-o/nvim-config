6347005e8319378e3df08bbb6c29da7873a80655
Jules Sang <jsang@hopia.eu>
2023-02-25 17:12:39 +0100

    Lazy: extract plugins into lua/plugins

--- init.lua
+++ init.lua
@@ -1,7 +1,40 @@
 vim.g.mapleader = " "
 
 
+-- TODO:
+--  - git difftool: installed diffview
+--  - barbar: display git modified files
+--  - Better code completion
+--  - ultisnips
+--  - Fold blocks
+--  - investigate on code lens
+--  - underline errors/Users/julessang/.local/state/nvim/lsp.log
+--
+--
+-- TOTEST:
+--  - lazy.nvim
+--
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup('plugins')
+
+
 require('config')
 require('remap')
-require('plugins')
 require('set')
+
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,33 +1,4 @@
--- TODO:
---  - git difftool: installed diffview
---  - barbar: display git modified files
---  - Better code completion
---  - ultisnips
---  - Fold blocks
---  - investigate on code lens
---  - underline errors/Users/julessang/.local/state/nvim/lsp.log
---
---
--- TOTEST:
---  - lazy.nvim
---
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-return require('lazy').setup({
+return {
 
     -- use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
@@ -151,4 +122,4 @@ return require('lazy').setup({
         'kosayoda/nvim-lightbulb',
         dependencies = 'antoinemadec/FixCursorHold.nvim',
       },
-    })
+    }
